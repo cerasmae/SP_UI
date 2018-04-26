fe7a48be9bd14d63bbf1607dc6b7630c93a19c80
Mae Celine Erasmo <cerasmae@users.noreply.github.com>
2018-04-26 12:19:36 +0800

    SP_UI upload

---
+++ SP_UI/views.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+	return render(request, 'index.html')
+
+
+def tables(request):
+	return render(request, 'tables.html')
